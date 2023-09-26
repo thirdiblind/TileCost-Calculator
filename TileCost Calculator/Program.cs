ed971c58e060b04704585cbb4d63eb017be485c9
Raymond Hart <z3nithvip3r@gmail.com>
2023-09-25 22:55:37 -0500

    Removed magic numbers Set constants for room shapes, PI, AREA_PER_HOUR and HOURLY_RATE Reorganized and cleaned up code

--- TileCost Calculator/Program.cs
+++ TileCost Calculator/Program.cs
@@ -5,43 +5,53 @@ namespace TileCost_Calculator
 {
     internal class Program
     {
+
+        const float PI = (float)Math.PI; // For the circle area calculation
+        const float AREA_PER_HOUR = 20f; // Units of area one can work in an hour
+        const float HOURLY_RATE = 86f; // Hourly labor rate
+
+        const int RECTANGULAR_ROOM = 1;
+        const int NON_RECTANGULAR_ROOM = 2;
+        const int TRIANGULAR_ROOM = 1;
+        const int CIRCULAR_ROOM = 2;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, this is a Tile Cost Calculator. Calculate the cost of flooring based on total area, labor and material cost.");
+            //Declaring float 
+            float area = 0;
 
+            Console.WriteLine("Hello, this is a Tile Cost Calculator. Calculate the cost of flooring based on total area, labor and material cost.");
+            
             //Set shape of room
             Console.WriteLine();
             Console.WriteLine("To start, select a room shape:");
             Console.WriteLine("1. For rectangular rooms.");
             Console.WriteLine("2. For non-rectangular rooms.");
             Console.Write("Enter your choice (1 or 2): ");
-            string shape = Console.ReadLine();
-            int roomShape = Int32.Parse(shape);
-            float area = 0;
+            int roomShape = Int32.Parse(Console.ReadLine());
+            
 
             //Error handling for when option 1 or 2 is NOT selected.. 
-            if (roomShape == 1 || roomShape == 2)
+            if (roomShape == RECTANGULAR_ROOM || roomShape == NON_RECTANGULAR_ROOM)
             {
 
                 //Rectangular room calcuation
-                if (roomShape == 1)
+                if (roomShape == RECTANGULAR_ROOM)
                 {
                     //Set Width
                     Console.Write("Enter the width of the room: ");
-                    string widthInput = Console.ReadLine();
-                    int width = Int32.Parse(widthInput);
+                    int width = Int32.Parse(Console.ReadLine());
 
                     //Set Length
                     Console.Write("Enter the lenth of room: ");
-                    string lengthInput = Console.ReadLine();
-                    int length = Int32.Parse(lengthInput);
+                    int length = Int32.Parse(Console.ReadLine());
 
                     //Calculate area of the rectangular room
                     area = width * length;
                 }
 
                 //Non-Rectangular room calculations (Triangle and Circle)
-                if (roomShape == 2)
+                if (roomShape == NON_RECTANGULAR_ROOM)
                 {
                     //Set shape of non-rectangular room
                     Console.WriteLine();
@@ -50,69 +60,58 @@ namespace TileCost_Calculator
                     Console.WriteLine("1. For triangular rooms.");
                     Console.WriteLine("2. For circular rooms.");
                     Console.Write("Enter your choice (1 or 2): ");
-                    string nonRectShape = Console.ReadLine();
-                    int nonRectRoomShape = Int32.Parse(nonRectShape);
+                    int nonRectRoomShape = Int32.Parse(Console.ReadLine());
                     
                     //Did not implement error handling here
 
                     //Triangular room calcuation
-                    if (nonRectRoomShape == 1)
+                    if (nonRectRoomShape == TRIANGULAR_ROOM)
                     {
                         Console.WriteLine("To calculate the area of a triangle, width must by multipled by length, then dividied by 1/2. ");
                         Console.WriteLine();
+
                         //Set Width
                         Console.Write("Enter the width of the room: ");
-                        string width = Console.ReadLine();
-                        int widthTriangle = Int32.Parse(width);
+                        int widthTriangle = Int32.Parse(Console.ReadLine());
 
                         //Set Length
                         Console.Write("Enter the lenth of room: ");
-                        string length = Console.ReadLine();
-                        int lengthTriangle = Int32.Parse(length);
+                        int lengthTriangle = Int32.Parse(Console.ReadLine());
 
                         //Calculate area of the triangle
-                        area = lengthTriangle * lengthTriangle;
+                        area = (widthTriangle * lengthTriangle) / 2;
                     }
 
                     //Circle room calcuation
-                    if (nonRectRoomShape == 2)
+                    if (nonRectRoomShape == CIRCULAR_ROOM)
                     {
                         Console.WriteLine("To calculate the area of a circle, pie is multipled by radius squared. ");
 
                         //Set Radius
                         Console.Write("Enter the radius of the circular room: ");
-                        string radius = Console.ReadLine();
-                        float radiusCircle = float.Parse(radius);
+                        float radiusCircle = float.Parse(Console.ReadLine());
 
                         //Calculate area of the circle
-                        area = (float)(radiusCircle * 2 * 3.14);
+                        area = (radiusCircle * 2 * PI);
                     }
                 }
 
                 //Set cost per unit
-                Console.Write("Enter the cost per unit of flooring for room: ");
-                string cost = Console.ReadLine();
-                float costPerUnit = float.Parse(cost);
+                Console.Write("Enter the cost per unit of flooring for the room: ");
+                float costPerUnit = float.Parse(Console.ReadLine());
 
-                //Calculate cost of materials
+                //Calculate all costs
                 float materialCost = area * costPerUnit;
+                float laborHours = area / AREA_PER_HOUR;
+                float laborCost = laborHours * HOURLY_RATE;
+                float totalCost = laborCost + materialCost;
 
-                //Output cost of materials
+                //Output all costs
                 Console.WriteLine();
                 Console.WriteLine("Cost of materials: $" + materialCost);
                 Console.WriteLine();
-
-                //Calculate labor cost based on hours of labor required
-                float laborHours = area / 20;
-                float hourlyRate = 86; // Hourly rate of $20 per hour
-                float laborCost = laborHours * hourlyRate;
-
-                //Output cost of labor
                 Console.WriteLine("Cost of labor: $86/hr * " + laborHours + " hours = $" + laborCost);
                 Console.WriteLine();
-
-                //Output Total Cost
-                float totalCost = laborCost + materialCost;
                 Console.WriteLine("Total cost: $" + totalCost);
             }
             else
